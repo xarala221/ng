c05803c2ca6411a5997529de29f2f91485734164
=xarala221 <xaralaxarala@gmail.com>
2018-07-18 11:27:31 +0000

    Changer le design pour le web et ajouter le login et le register form

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,8 +3,8 @@
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
+  $routeProvider.when('/!view1', {
+    templateUrl: '../index.html',
     controller: 'View1Ctrl'
   });
 }])
